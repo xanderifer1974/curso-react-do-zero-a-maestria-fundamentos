1ddd8478736e2e2f0dd7f05303bfb64db614755d
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-22 14:13:28 -0300

    Exemplo de comentário

--- src/components/FirstComponents.js
+++ src/components/FirstComponents.js
@@ -1,6 +1,8 @@
+//Exemplo de comentário fora do JSX
 const FirstComponent = () => {
   return (
     <div>
+        {/*Primeiro componente do curso de React - Exemplo de comentário dentro do JSX */}
       <h1>Meu primeiro componente!</h1>
     </div>
   );
